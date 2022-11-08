98e80a0067044aca30e589859fc9c85fbc9c3195
willtreuel6 <willtreuel6@gmail.com>
2022-11-08 11:21:42 -0500

    Initial commit

---
+++ src/main/java/com/willtreuel/cardshopapi/CardShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.willtreuel.cardshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardShopApiApplication {
+
+	public static void main(String[] args) {SpringApplication.run(CardShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willtreuel/cardshopapi/CardShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willtreuel.cardshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
